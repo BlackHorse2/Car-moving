309b1f64b93cba401ae36b5fe2c58fecd28965f8
BlackHorse2 <59952754+BlackHorse2@users.noreply.github.com>
2020-01-16 13:48:37 +0600

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,37 @@
+#include <graphics.h>
+#include <dos.h>
+#include <conio.h>
+
+main()
+{
+    int i, j = 0, gd = DETECT, gm;
+
+    initgraph(&gd,&gm,"C:\\TC\\BGI");
+
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+    outtextxy(25,240,"Press any key to view the moving car");
+
+    getch();
+    setviewport(0,0,639,440,1);
+
+    for (i = 0; i <= 420; i = i + 10, j++)
+    {
+        setcolor(WHITE);
+        rectangle(50+i,275,150+i,400);
+        rectangle(150+i,350,200+i,400);
+        setcolor(YELLOW);
+        circle(75+i,410,10);
+        circle(175+i,410,10);
+
+        delay(60);
+
+        if (i == 420)
+            break;
+
+        clearviewport();
+    }
+
+    getch();
+    closegraph();
+    return 0;
+}
